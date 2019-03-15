fced7066027729ce4781d0c004dc1be3bc5f3d73
josiahconely <josiahconely@gmail.com>
2019-03-15 17:30:14 -0500

    pics

--- IPC 8/DeepLearning_Lesson1/ICP 8 part 1.py
+++ IPC 8/DeepLearning_Lesson1/ICP 8 part 1.py
@@ -13,14 +13,15 @@ X_train, X_test, Y_train, Y_test = train_test_split(dataset[:,0:8], dataset[:,8]
                                                     test_size=0.25, random_state=87)
 #np.random.seed(155)
 
-for num in range (10, 35):
+num = 20
 
-    my_first_nn = Sequential() # create model
-    my_first_nn.add(Dense(num, input_dim=8, activation='relu')) # hidden layer
-    my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
-    my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    my_first_nn.fit(X_train, Y_train, epochs=100, verbose=0,
+my_first_nn = Sequential() # create model
+my_first_nn.add(Dense(num, input_dim=8, activation='relu')) # hidden layer
+my_first_nn.add(Dense(20, activation='sigmoid')) # hidden layer
+my_first_nn.add(Dense(1, activation='sigmoid')) # output layer
+my_first_nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+my_first_nn.fit(X_train, Y_train, epochs=100, verbose=0,
                                      initial_epoch=0)
-    #print(my_first_nn.summary())
-    print(my_first_nn.metrics_names)
-    print(num, ": ",my_first_nn.evaluate(X_test, Y_test, verbose=0))
+#print(my_first_nn.summary())
+print(my_first_nn.metrics_names)
+print(num, ": ",my_first_nn.evaluate(X_test, Y_test, verbose=0))
